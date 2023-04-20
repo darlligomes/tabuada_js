14d541ddae705248a182d050117742ce27221c09
Darllington Gomes <darllingtongomes3@gmail.com>
2023-04-20 14:36:22 -0300

    Atualização com modo noturnoe alteração de cores

--- app.js
+++ app.js
@@ -28,3 +28,20 @@ window.document.onmouseleave= function(){
 
 };
 }
+
+function toggleMode(){
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  if(html.classList.contains('light')){
+html.setAttribute('class', 'light');
+document.querySelector("ion-icon").setAttribute('name','sunny-outline');
+
+  } else{
+    html.setAttribute('class', 'dark');
+    document.querySelector("ion-icon").setAttribute('name','moon-outline');
+  }
+ 
+
+}
+
